7d5ea9fab6f382e166d23f302e6c2cb35f013b7e
Chan Gyun Im <ck91528@gmail.com>
2017-01-24 11:03:08 -0800

    tried my best,,,

---
+++ composite-dom.js
@@ -0,0 +1,13 @@
+"use strict";
+var DomElement = (function () {
+    function DomElement() {
+    }
+    return DomElement;
+}());
+exports.DomElement = DomElement;
+var TextNode = (function () {
+    function TextNode() {
+    }
+    return TextNode;
+}());
+exports.TextNode = TextNode;
